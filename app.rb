1d8528aa92de623458076c9375955070a9727dbb
Chris Kim <chrisdohyunkim@gmail.com>
2025-09-10 01:27:55 +0000

    Final draft before rake grade

--- app.rb
+++ app.rb
@@ -2,35 +2,49 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Howdy</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb(:homepage)
 end
 
 get("/rock") do
   moves = ["rock", "paper", "scissors"]
-  comp_move = moves.sample
+  @comp_move = moves.sample
 
-  if comp_move = "rock"
-    outcome = "tied"
-  elsif comp_move = "paper"
-    outcome = "lost"
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
   else
-    outcome = "won"
+    @outcome = "won"
+  end
+erb(:zebra)
+end
+
+get("/paper") do
+    moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
   end
 
-  "
-  <h2>
-    We played rock!
-  </h2>
+  erb(:giraffe)
+end
 
-  <h2>
-    They played #{comp_move}!
-  </h2>
+get("/scissors") do
+    moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
 
-  <h2>
-    We #{outcome}!
-  </h2>
-  "
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:lion)
 end
